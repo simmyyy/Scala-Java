729c17baba0336a13e13cf0e9d9af51792ef148e
simmyyy <jizewski1@gmail.com>
2019-01-22 11:32:45 -0800

    Adding Scala basics

--- src/main/scala/functional/composition/GenericCategory.scala
+++ src/main/scala/functional/composition/GenericCategory.scala
@@ -1,6 +1,15 @@
 package functional.composition
 
-trait GenericCategory[->>[_,_]] {
-  def id[A]: A ->> A
+trait GenericCategory[->>[_, _]] {
+  def id[A]: ->>[A, A]
+
   def compose[A, B, C](g: B ->> C, f: A ->> B): A ->> C
 }
+
+//someType -> [B,C]
+//someType -> [A,B]
+
+//trait GenericCategory[(A->B)] {
+//  def id[A]: A ->> A
+//  def compose[A, B, C](g: B ->> C, f: A ->> B): A ->> C
+//}
